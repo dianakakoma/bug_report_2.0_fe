33abce3e942316a8339c4dbbc0f448cd4cf1bb5a
dianakakoma <dikakoma@gmail.com>
2020-09-08 21:45:53 -0500

    created index and create action page for the front-end.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="report in reports">
+      <p>Report Id: {{report.id}}</p>
+       <p>Reported by: {{report.name}}</p>
+       <p>Description: {{report.description}}</p>
+       <br>
+      URL: <a v-bind:href="report.url">{{report.url}}</a>
+      <br>
+      <img v-bind:src="report.screenshot" v-bind:alt="Screenshot" width="200px"/>
+      <p>Suggested fix: {{report.suggested_fix}}</p>
+      <h2 style="color:red">****</h2>
+    </div>
   </div>
 </template>
 
@@ -8,13 +19,19 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Bug Report!",
+      message: "Welcome to Bug Report 2.0!",
+      reports: [],
     };
   },
-  created: function () {},
+  created: function () {
+    axios.get("/api/reports").then((response) => {
+      this.reports = response.data;
+    });
+  },
   methods: {},
 };
 </script>
